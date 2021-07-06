6ba21ae597231201e14e28b47d48a2eb25176a45
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 21:35:15 +0100

    Updates

--- Layouts/BlogLayout.js
+++ Layouts/BlogLayout.js
@@ -1,8 +1,23 @@
-const BlogLayout = ({children}) => {
+import Markdown from 'markdown-to-jsx';
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const BlogLayout = ({source, frontMatter}) => {
     return (
         <>
-            <div className="p-12 mx-auto max-w-5xl prose prose-green md:prose-lg lg:prose-xl text-gray-900 dark:text-gray-50">
-                {children}
+            <div className="bg-white dark:bg-gray-900">
+                <div className="App min-h-screen relative">
+                    <Navbar />
+                    <div className="px-12 py-24 mx-auto max-w-5xl prose prose-green md:prose-lg lg:prose-xl text-gray-900 dark:text-gray-50">
+                        <h1>
+                            {frontMatter.title}
+                        </h1>
+                        <div>
+                            <Markdown children={source} />
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
             </div>
         </>
     )
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,18 +1,11 @@
 import { getAllPostSlugs, getPostdata } from "../../Lib/mdx";
 import matter from "gray-matter";
-import Markdown from 'markdown-to-jsx';
+import BlogLayout from "../../Layouts/BlogLayout";
 
 export default function Posts({ source, frontMatter }) {
   return (
     <>
-      <div className="p-12 mx-auto max-w-5xl prose prose-green md:prose-lg lg:prose-xl text-gray-900 dark:text-gray-50">
-          <h1>
-            {frontMatter.title}
-          </h1>
-          <div>
-          <Markdown children={source} />
-          </div>
-      </div>
+      <BlogLayout source={source} frontMatter={frontMatter}/>
     </>
   );
 }
