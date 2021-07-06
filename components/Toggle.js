1f07a2937a1494a79c96601442359f9153ae617c
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 17:07:14 +0100

    Sounds config

--- components/Toggle.js
+++ components/Toggle.js
@@ -2,6 +2,9 @@ import { BiSun, BiMoon } from "react-icons/bi";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
+import useSound from 'use-sound';
+import Click from '../publix/sounds/click.mp3'
+
 const Toggle = () => {
     const [isMounted, setIsMounted] = useState(false);
     const { theme, setTheme } = useTheme();
@@ -16,10 +19,12 @@ const Toggle = () => {
         }
     };
 
+    const [play] = useSound(Click);
+
     return (
         <button
               className="flex items-center mx-2 text-base text-gray-800 hover:text-indigo-600 dark:text-gray-50"
-              onClick={toggleTheme}
+              onClick={toggleTheme | play}
             >
               <span className="text-lg">
                 {isMounted && theme === "dark" ? <BiMoon /> : <BiSun />}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  reactStrictMode: true,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'url-loader',
+      },
+    });
+    return config;
+  },
 }
