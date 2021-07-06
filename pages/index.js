1a03afe9299b58d14b6ba8805bc378c3c93464d1
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 14:39:09 +0100

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/globals.css'
 import Header from '../components/Header'
 
 const index = () => {
   return (
     <body>
+      
       <Header />
       {/* <Hero /> */}
     </body>
