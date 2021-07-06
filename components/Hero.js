85d3f6a7fc1ae2e9a418874fcd3adb64bb72be4f
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 14:57:59 +0100

    Tag changes

--- components/Hero.js
+++ components/Hero.js
@@ -23,3 +23,5 @@ const Hero = () => {
         </>
     )
 }
+
+export default Hero
---
+++ components/Tag.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Tag = ({tag, link}) => {
+    return (
+        <>
+            <Link href={link}>
+                <div 
+                    className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-300 dark:bg-green-500 text-grey-300 rounded-full m-1">
+                    {tag}
+                </div>
+            </Link>
+        </>
+    )
+}
+
+export default Tag
