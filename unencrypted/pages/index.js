3496127581bce4d5ccd3e32325d46a1fe1fbb45a
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 14:24:24 +0100

    Updates

---
+++ unencrypted/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <body>
+      
+    </body>
+  )
+}
+
+export default index
