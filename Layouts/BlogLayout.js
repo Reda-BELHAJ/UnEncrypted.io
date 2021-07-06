2bf6173d1f55efd275e939db22085e400f62d43d
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-06 22:45:35 +0100

    Updates Layer

--- Layouts/BlogLayout.js
+++ Layouts/BlogLayout.js
@@ -2,6 +2,7 @@ import Markdown from 'markdown-to-jsx';
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import BlogShare from '../components/BlogShare';
+import Tag from '../components/Tag'
 
 const BlogLayout = ({source, frontMatter}) => {
     return (
@@ -10,15 +11,27 @@ const BlogLayout = ({source, frontMatter}) => {
                 <div className="min-h-screen relative">
                     <Navbar />
                     <div className="py-24">
-                        <div className="py-5 max-w-4xl mx-auto rounded-lg shadow-lg bg-white dark:bg-gray-900 px-12 prose prose-green md:prose-lg lg:prose-xl text-gray-900 dark:text-gray-50">
-                            <h1>
-                                {frontMatter.title}
-                            </h1>
-                            <div>
-                                <Markdown children={source} />
+                        <div className="py-5 max-w-4xl mx-auto rounded-lg shadow-lg bg-white dark:bg-gray-900 px-12">
+                            <img
+                                className="object-cover w-full h-72"
+                                src={frontMatter.image}
+                                alt="Article Image"
+                            />
+
+                            {
+                                <Tag link="/" tag="TryHackMe"/>
+                            }
+
+                            <div className="prose prose-green md:prose-lg lg:prose-xl text-gray-900 dark:text-gray-50">
+                                <h1>
+                                    {frontMatter.title}
+                                </h1>
+                                <div>
+                                    <Markdown children={source} />
+                                </div>
                             </div>
                         </div>
-
+                        
                         <BlogShare />
                     </div>
                     
