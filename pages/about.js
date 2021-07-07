3bac4c1776a3e27a363625bf48e492805c6c2781
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-07 15:55:01 +0100

    Updates

--- pages/about.js
+++ pages/about.js
@@ -3,17 +3,22 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import About from '../components/About'
 import Tracks from '../components/Tracks'
+import Head from "next/head";
 
 const about = () => {
     return (
-        <div>
+        <>
+            <Head>
+                <title>UnEncrypted</title>
+                <meta itemProp="name" content="UnEncrypted" />
+            </Head>
             <div className="min-h-screen relative bg-white dark:bg-gray-900">
                 <Navbar />
                 <About />
                 <Tracks />
                 <Footer />
             </div>
-        </div>
+        </>
     )
 }
 
