1dd0d7ba60fed4c250aacbd76e76755ed16481c8
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-08 16:55:55 +0100

    Updates

--- Lib/tags.js
+++ Lib/tags.js
@@ -1,26 +1,41 @@
 import fs from 'fs'
-import matter from 'gray-matter'
-import path from 'path'
 import { getAllBlogPosts } from './mdx'
 
-export async function getAllTags(type) {
-  const blogs = await getAllBlogPosts();
+export async function getAllTags() {
+    const blogs = await getAllBlogPosts();
 
-  let tagCount = {}
-  // Iterate through each post, putting all found tags into `tags`
-  blogs.forEach(
-      (blog) => blog.data.tags.forEach(
-          (tag) => {
-              const tagUpp = tag.toUpperCase()
-              if (tagUpp in tagCount){
-                tagCount[tagUpp] += 1
-              }
-              else {
-                tagCount[tagUpp] = 1
-              }
-          }
-      )
-  )
+    let tagCount = {}
+    
+    blogs.forEach(
+        (blog) => blog.data.tags.forEach(
+            (tag) => {
+                const tagUpp = tag.toUpperCase()
+                if (tagUpp in tagCount){
+                    tagCount[tagUpp] += 1
+                }
+                else {
+                    tagCount[tagUpp] = 1
+                }
+            }
+        )
+    )
 
-  return tagCount
+    var sortable = [];
+    for (var tag in tagCount) {
+        sortable.push([tag, tagCount[tag]]);
+    }
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    return sortable
 }
+
+export function getCatBlog({tag}){
+    const Blogs = getAllBlogPosts();
+    
+    return Blogs.filter(
+      blog => blog.data.tags.includes(tag)
+    )
+  }
