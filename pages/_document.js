74c9dc6275ef069148cc1a9697acdfe016686750
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-13 23:24:58 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                     <meta name="robots" content="index"/>
-                    <meta name="keywords" content="UnEncrypted, Reda BELHAJ, Next.JS, Blog, PortFolio, WriteUps, Walkthrough, CTF, Reda, BELHAJ" />
+                    <meta name="keywords" content="UnEncrypted, Reda BELHAJ, Next.JS, Blog, PortFolio, WriteUps, Walkthrough, CTF, Reda, BELHAJ, Github, Vercel, UIR, Universite Internationale de Rabat, Training, Courses, Challenges, Git, Examples" />
                     <meta name="author" content="Reda BELHAJ" />
                     <link rel="apple-touch-icon" href="/favicon.ico" />
                 </Head>
