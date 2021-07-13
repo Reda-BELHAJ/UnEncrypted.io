2b21862a7478fc67a52db627572daed420045d3b
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-13 19:24:08 +0100

    Updates

---
+++ pages/api/sitemap.js
@@ -0,0 +1,47 @@
+import { getAllPostSlugs } from '../../Lib/mdx'
+
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+
+export default async (req, res) => {
+    try {
+        // An array with your links
+        const links = [];
+        getAllPostSlugs().map((post) => {
+            links.push({
+            url: `/blog/${post.params.slug}`,
+            changefreq: "daily",
+            priority: 0.9,
+            });
+        });
+    
+        // Add other pages
+        const pages = ["/about", "/tags", "/blog"];
+        pages.map((url) => {
+            links.push({
+            url,
+            changefreq: "daily",
+            priority: 0.9,
+            });
+        });
+    
+        // Create a stream to write to
+        const stream = new SitemapStream({
+            hostname: `https://${req.headers.host}`,
+        });
+    
+        res.writeHead(200, {
+            "Content-Type": "application/xml",
+        });
+    
+        const xmlString = await streamToPromise(
+            Readable.from(links).pipe(stream)
+        ).then((data) => data.toString());
+    
+        res.end(xmlString);
+        
+    } catch (e) {
+        console.log(e);
+        res.send(JSON.stringify(e));
+        }
+};
