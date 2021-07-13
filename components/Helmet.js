07316e9e5198b71b795517e1aee97fde60a05ce3
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-13 16:58:23 +0100

    Updates

--- components/Helmet.js
+++ components/Helmet.js
@@ -1,12 +1,13 @@
-
+import { useEffect } from "react"
 
 const Helmet = ({title, description, imageURL, imageALT}) => {
+
     return (
         <>
             <meta property="og:title" content={title}/>
             <meta property="og:description" content={description} />
             <meta property="og:image" content={imageURL}/>
-            <meta property="og:url" content={window.location.href}/>
+            <meta property="og:url" content="https://unencrypted.vercel.app/"/>
             <meta name="twitter:card" content="summary_large_image" />
 
             <meta property="og:site_name" content="European Travel, Inc." />
