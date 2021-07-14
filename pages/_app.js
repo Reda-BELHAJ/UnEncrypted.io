c2cde671b4cf6e604a7397aa4a256b02572f9fb7
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-14 12:09:20 +0100

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../Lib/gtag'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GA_TRACKING_ID } from '../Lib/gtag'
 
 class MyDocument extends Document {
     render() {
@@ -10,6 +11,22 @@ class MyDocument extends Document {
                     <meta name="keywords" content="UnEncrypted, Reda BELHAJ, Next.JS, Blog, PortFolio, WriteUps, Walkthrough, CTF, Reda, BELHAJ, Github, Vercel, UIR, Universite Internationale de Rabat, Training, Courses, Challenges, Git, Examples" />
                     <meta name="author" content="Reda BELHAJ" />
                     <link rel="apple-touch-icon" href="/favicon.ico" />
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
