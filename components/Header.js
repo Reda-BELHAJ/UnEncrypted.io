b585f3f0144c1911f49ba683d753ecdb0b83f310
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-22 16:45:47 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -15,20 +15,19 @@ const Header = () => {
             }
         },
     }
+
     return (
         <>
             <div className="pt-24 px-12 mx-auto max-w-7xl">
                 <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
                     <h1 className="mb-8 text-4xl font-bold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight dark:text-gray-50">
-                    <motion.div initial="hidden" animate="visible" variants={variants}>
-                        <span>Decide</span>{" "}
-                        <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-100 to-green-800 via-green-500 animate-gradient-x lg:inline">
-                            Learn
-                        </span>{" "}
-                        <span>Share 🔗</span>
-                    </motion.div>
-                    
-
+                        <motion.div initial="hidden" animate="visible" variants={variants}>
+                            <span>Decide</span>{" "}
+                            <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-100 to-green-800 via-green-500 animate-gradient-x lg:inline">
+                                Learn
+                            </span>{" "}
+                            <span>Share 🔗</span>
+                        </motion.div>
                     </h1>
 
                     <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24 dark:text-gray-400">
