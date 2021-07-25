61064957d1422a00a716253523919b5030659a63
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-07-25 18:46:22 +0100

    Update Tracks.js

--- components/Tracks.js
+++ components/Tracks.js
@@ -5,7 +5,7 @@ const Tracks = () => {
     return (
         <>
             <div>
-                <div className="pb-20 px-10 mx-auto max-w-7xl">
+                <div className="px-10 mx-auto max-w-7xl">
                     <div className="pt-6 pb-8 space-y-2 md:space-y-5">
                             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                                 Top Tracks
