5160ba3a2879d87348441c040b64c5b54c745345
Reda BELHAJ <58605073+Reda-BELHAJ@users.noreply.github.com>
2022-01-22 13:07:07 +0100

    Update BlogContent.js

--- Layouts/BlogContent.js
+++ Layouts/BlogContent.js
@@ -12,7 +12,7 @@ const BlogContent = ({title}) => {
                 className="mb-5 grid grid-cols-7">
                     
                 <div 
-                    className="inline-block leading-relaxed align-middle">
+                    className="inline-block leading-relaxed">
                     <Link 
                         href={`/blog/${String(
                             title.split(" ").join("-").toLowerCase()
@@ -20,7 +20,7 @@ const BlogContent = ({title}) => {
                         <a 
                             aria-label={title} 
                             className="text-green-300 dark:text-green-500">
-                            <TiArrowRightThick />
+                            <TiArrowRightThick className="inline-block align-middle" />
                         </a>
                     </Link>
                 </div>
